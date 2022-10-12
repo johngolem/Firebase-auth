0dfcb03fee0bdde951f2f3f7af15ab233a7e75ec
John Paul <johnpaul@trees.local>
2022-10-12 06:25:19 +0300

    use conditional rendering to redirect user based on login status

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,23 +1,32 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase";
+import { useRouter } from "next/router";
 
 export default function Dashboard() {
   const [user, loading] = useAuthState(auth);
+  const route = useRouter();
+
+
+//   conditionalrendering
+
+
 
   if (loading) return <h1>still loading..........</h1>;
-  //   if (!user)
-  return (
-    <div>
-      <h1>
-        Welcome to your dashboard
-        {user.displayName}
-      </h1>
-      <button
-        className="mt-20 py-2 px-4 text-lg bg-red-500 text-white rounded-lg font-medium ml-8"
-        onClick={() => auth.signOut()}
-      >
-        Signout
-      </button>
-    </div>
-  );
+  if (!user) route.push("/auth/login");
+  if (user)
+    return (
+      <div>
+        <h1>
+          Welcome to your dashboard
+          {user.displayName}
+        </h1>
+        <button
+          className="mt-20 py-2 px-4 text-lg bg-red-500 text-white rounded-lg font-medium ml-8"
+          onClick={() => auth.signOut()}
+        >
+          Signout
+        </button>
+      </div>
+    );
 }
+
